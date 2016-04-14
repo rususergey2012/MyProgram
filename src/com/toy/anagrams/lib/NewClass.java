ae1981a9874f98856b2e28c2ca68f69d837cf75f
rususergey2012 <rususergey2012@gmail.com>
2016-04-15 00:07:34 +0300

    MyProgram Proba #1.2

---
+++ src/com/toy/anagrams/lib/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toy.anagrams.lib;
+
+/**
+ *
+ * @author sergey
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("hello my friend how are you ?");
+    }
+ 
+}
